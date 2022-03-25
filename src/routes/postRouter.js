245b00073a678f26993aa5aaaefc267813f0cf5c
kethllen <kety_andrey@hotmail.com>
2022-03-25 18:33:05 -0300

    Users controllers

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,13 +1,28 @@
 import { Router } from "express";
-import { deletePost, editPost, getPosts, publishPost } from "../controllers/postController.js";
+import {
+  deletePost,
+  editPost,
+  getPosts,
+  publishPost,
+} from "../controllers/postController.js";
 import validaTokenMiddleware from "../middlewares/validaTokenMiddleware.js";
 import validSchema from "../middlewares/validSchema.js";
 import postSchema from "../schemas/postSchema.js";
 
 const postRouter = Router();
-postRouter.post('/posts', validSchema(postSchema), validaTokenMiddleware, publishPost);
-postRouter.get('/posts', getPosts);
-postRouter.delete('/posts/:postId', validaTokenMiddleware, deletePost);
-postRouter.put('/posts/:postId', validSchema(postSchema), validaTokenMiddleware, editPost);
+postRouter.post(
+  "/posts",
+  validSchema(postSchema),
+  validaTokenMiddleware,
+  publishPost
+);
+postRouter.get("/posts", getPosts);
+postRouter.delete("/posts/:postId", validaTokenMiddleware, deletePost);
+postRouter.put(
+  "/posts/:postId",
+  validSchema(postSchema),
+  validaTokenMiddleware,
+  editPost
+);
 
 export default postRouter;
