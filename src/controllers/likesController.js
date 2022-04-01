8569344ff368227eb513bfe07f36e565fa1b6dd9
kethllen <kety_andrey@hotmail.com>
2022-04-01 16:16:15 -0300

    like

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -20,26 +20,6 @@ export async function toggleLikedPost(req, res) {
   try {
     const { postId } = req.params;
     const { id } = res.locals.user;
-
-    const repost = await checkRepostId(Number(postId));
-
-    if (repost.rows[0].repostId === null) {
-      const isUserLiked = await searchIfUserLiked(id, postId);
-      if (isUserLiked.rowCount) {
-        await removeLike(id, postId);
-      } else {
-        await insertLike(id, postId);
-      }
-    } else {
-      const isUserLiked = await searchIfUserLiked(id, repost.rows[0].repostId);
-
-      if (isUserLiked.rowCount) {
-        await removeLike(id, postId);
-      } else {
-        await insertLike(id, postId);
-      }
-    }
-
     const isUserLiked = await searchIfUserLiked(id, postId);
 
     if (isUserLiked.rowCount) {
