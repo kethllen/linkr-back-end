fdda6f57fce44cd533d06bcc076266e04ef8f968
abellasjuan <juanvictoraa@gmail.com>
2022-04-01 15:04:55 -0300

    feat: verify if user is been followed

--- src/routes/followersRouter.js
+++ src/routes/followersRouter.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { checkFollowers, getFollowers, toggleFollowingUser } from "../controllers/followersController.js";
+import { checkFollowers, getFollowers, toggleFollowingUser, verifyIfIsCurrentFollower } from "../controllers/followersController.js";
 import validaTokenMiddleware from '../middlewares/validaTokenMiddleware.js';
 
 const followersRouter = Router();
 followersRouter.get("/followers", getFollowers);
 followersRouter.post("/followers/:visitedUserId", validaTokenMiddleware, toggleFollowingUser);
 followersRouter.get("/userfollows", validaTokenMiddleware, checkFollowers)
+followersRouter.get("/followers/:visitedUserId", validaTokenMiddleware, verifyIfIsCurrentFollower);
+
 export default followersRouter;
